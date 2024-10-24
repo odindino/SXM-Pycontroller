12b513db4b75512a38e43b962dafc46248acfc81
odinswiss <F10245003@ntu.edu.tw>
2024-10-25 02:05:56 +0800

    update files

--- SXMPycontroller.py
+++ SXMPycontroller.py
@@ -1,7 +1,17 @@
 # This file is the to make a user-friendly module for SXM controlling
+# Scenarios:
+# 1. Conduct a spectroscopy measurement(point, line or array) at a given position with varying Vds or Vg bias
+# 1-1. After moving the tip to the position, turn off the feedback loop
+# 1-2. Conduct the command for SMU, set the Vds or Vg bias, and turn on the SMU
+# 1-3. Conduct the spectroscopy measurement
+# 1-4. Turn off the SMU
+# 1-5. Turn on the feedback loop
+# 1-6. Repeat the above steps for different Vds or Vg bias, if any.
+# 1-7. Move the tip to the next position and repeat the above steps.
 from RemoteSXM import SXMRemote
 import time
 import pyvisa
+import ksb2902bsmu as smu
 
 
 class SXMController:
