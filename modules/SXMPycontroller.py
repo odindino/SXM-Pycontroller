4299c9a029e5bf82b85f37161926bf438c7fa701
odinswiss <F10245003@ntu.edu.tw>
2024-11-27 13:03:11 +0800

    add zoffset setting

--- modules/SXMPycontroller.py
+++ modules/SXMPycontroller.py
@@ -181,6 +181,7 @@ class SXMController(SXMCITSControl):
                 'ch2_output': False, 
                 'ch2_voltage': 0.0
             }
+            original_zoffset = self.get_zoffset()
             
             # 檢查並記錄SMU狀態
             for ch in [1, 2]:
@@ -204,6 +205,9 @@ class SXMController(SXMCITSControl):
                     time.sleep(0.5)  # 等待output穩定
             
             # 關閉回饋
+            if original_zoffset != 0.0:
+                self.set_zoffset(0.0)
+
             self.feedback_off()
             time.sleep(0.5)  # 等待系統穩定
             
@@ -256,8 +260,10 @@ class SXMController(SXMCITSControl):
                         self.smu.disable_output(Channel(ch))
                 
                 # 恢復回饋狀態
-                if original_states['feedback']:
+                if original_states['feedback'] != self.FbOn:
                     self.feedback_on()
+                    self.set_zoffset(original_zoffset)
+
                 
             except Exception as e:
                 print(f"Error restoring original states: {str(e)}")
